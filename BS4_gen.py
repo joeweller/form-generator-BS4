10766b73361347ddd3a8a12c7b8f9911e8ef8fe8
necpock-laptop <necpock@gmail.com>
2018-08-25 00:12:00 +0100

    text pretty much working

--- BS4_gen.py
+++ BS4_gen.py
@@ -1,23 +1,83 @@
-class Bs4Gen:
+class BS4Gen:
+
     def __init__(self, OPTIONS):
         self.options = OPTIONS
+        self.output = ''
+
+        # strip whitespace for all options
+        for key, val in self.options.items():
+            self.options[key] = val.strip()
+
+        if self.options['type'] == 'text':  # for text input type
+            self.type_text()
+        elif self.options['btn-type']:  # for button types
+            self.type_button()
+
+        return
+
+    def type_button(self):
+        """return a button"""
+
 
     def type_text(self):
         """return a full text widget"""
 
-        # check if label is required. if so, generate a label
-        if self.options['label']:
-            label = self.label()
+        widget = '<input type="{text_type}" class="form-control{size}" id="{id}" {placeholder}>'.format(
+            size=' {}'.format(self.return_size_class('form-control-')) if not self.options['size'] == 'regular' else '',
+            text_type=self.options['text-type'],
+            placeholder=self.return_placeholder() if self.options['placeholder'] else '',
+            id=self.options['id']
+        )
+        self.output = self.format_html(
+            [
+                self.label() if self.options['label'] else None,  # return label element
+                widget,  # pass in widget
+                self.return_small_hint() if self.options['small-hint'] else None,  # return small hint element
+             ])
+        return
 
-        size = self.size()
+    def get_widget(self):
+        """return complete widget"""
+        return self.output
 
+    @staticmethod
+    def format_html(element_list):
+        """finalize widget for final output"""
+        elements = ''
 
-    def size(self):
+        for e in element_list:
+            if not e:
+                pass
+            else:
+                elements = elements + '  {ele}\n'.format(ele=e.strip())
+
+        return '<div class ="form-group">\n{ele}</div>\n'.format(ele=elements)
+
+    def return_size_class(self, class_prefix):
         """return the class for input size"""
-        if not self.options['size']:
+        if self.options['size'] == 'regular':
             return ''
+        elif self.options['size'] == 'large':
+            tmp = 'lg'
+        elif self.options['size'] == 'small':
+            tmp = 'sm'
+        return '{pref}{size}'.format(pref=class_prefix, size=tmp)
+
+    def return_small_hint(self):
+        """create small hint html"""
+        if self.options['small-hint']:
+            return '<small id="{id}Help" class="form-text text-muted">{small_hint}</small>'.format(
+                small_hint=self.options['small-hint'],
+                id=self.options['id']
+            )
+        return None
+
+    def return_placeholder(self):
+        """return placeholder tag"""
+        if self.options['placeholder']:
+            return 'placeholder="{placeholder}"'.format(placeholder=self.options['placeholder'])
         else:
-            return 'form-control-{}'.format(self.options['size'])
+            return ''
 
     def label(self):
         """return a label"""
@@ -25,11 +85,3 @@ class Bs4Gen:
             id=self.options['id'],
             label=self.options['label']
         )
-
-    #def placeholder(self):
-    #    if self.options['placeholder'] !=
-
-        #<div class ="form-group">
-        #<label for="formGroupExampleInput"> Example label </label>
-        #<input type = "text" class ="form-control" id="formGroupExampleInput" placeholder="Example input">
-        #</div>
--- cli.py
+++ cli.py
@@ -1,51 +1,123 @@
-import BS4_gen
+from BS4_gen import BS4Gen
+import os
+
+collected = []  # list of options selected for viewing
 
 OPTIONS = {
-        'size': '',  # sm = 'form-control-sm', re = '', lg = 'form-control-lg'
+        'size': '',  # small = 'form-control-sm', regular = '', large = 'form-control-lg'
         'placeholder': '',  # text for placeholder
         'label': '',  # text for label
         'type': '',  # for function call to relevent type
-        'text-type': ''  # second type for 'text': password, email, text
+        'text-type': '',  # second type for 'text': password, email, text
+        'id': '',  # widget id
+        'small-hint': ''
     }
+
 
 def main():
 
-    print("BS4 Form Widget Generator.")
+    print("BS4 Widget Generator")
 
     get_type()
-
+    get_placeholder()
+    get_label()
+    get_id()
+    get_size()
+    get_small_hint()
+    display_collected()
+    print('output:\n\n')
+    output = BS4Gen(OPTIONS).get_widget()
+    print(output)
     return
 
+
+def get_small_hint():
+    """get input for small hint"""
+    inp = option_text('Input "small" hint (leave blank for no hint)')
+    add_to_collected('small hint', inp)
+    OPTIONS['small-hint'] = inp
+    return
+
+
+def get_size():
+    """get size for widget"""
+    opt = ['regular', 'small', 'large']
+    inp = option_menu(opt, 'Widget size:')
+    OPTIONS['size'] = opt[inp]
+    add_to_collected('size', opt[inp])
+    return
+
+
+def get_label():
+    """get label for widget. '' if not widget required"""
+    inp = option_text('Input label name (leave blank for no label):')
+    add_to_collected('label', inp)
+    OPTIONS['label'] = inp
+    return
+
+
+def get_id():
+    """get id for widget"""
+    inp = option_text('Input widget id:')
+    add_to_collected('id', inp)
+    OPTIONS['id'] = inp
+    return
+
+
+def get_placeholder():
+    """get placeholder"""
+    inp = option_text('Input placeholder (leave blank for no placeholder):')
+    add_to_collected('placeholder', inp)
+    OPTIONS['placeholder'] = inp
+    return
+
+
 def get_type():
-    """display menu and get widget type"""
-    opt = ['text',]
-    inp = display_menu(opt, 'Select Widget:')
+    """get widget type"""
+    opt = ['text', ]
+    inp = option_menu(opt, 'Select widget:')
 
     OPTIONS['type'] = opt[inp]  # set option
 
-    if OPTIONS['type'] == 'text':
-        get_texttype()
+    add_to_collected('type', opt[inp])
 
+    if OPTIONS['type'] == 'text':
+        get_text_type()
     return
 
-def get_texttype():
-    opt = ['text', 'email', 'password']
-    inp = display_menu(opt, 'Select text type:')
 
+def get_text_type():
+    """get type of text widget"""
+    opt = ['text', 'email', 'password']
+    inp = option_menu(opt, 'Select text type:')
+
+    # mark text type with option
     OPTIONS['text-type'] = opt[inp]
 
+    # add option to collected list
+    add_to_collected('text type', opt[inp])
+
     return
 
-def display_menu(opt_list, title):
+
+def option_menu(opt_list, title):
     """Generate a menu"""
+    if collected:
+        display_collected()
+
     print(title)
     count = 0
     for o in opt_list:
         print('({number}) : {option}'.format(number=count, option=o))
-    return input_loop(opt_list.count)
+        count += 1
 
-def input_text(message=''):
+    return input_loop(len(opt_list))
+
+
+def option_text(message=''):
+    display_collected()
     return input(message)
+
 
 def input_loop(menu_range):
     """Loop input for options menus"""
@@ -56,15 +128,40 @@ def input_loop(menu_range):
         except ValueError:
             return False
 
-        if int(chk) in range(0, rng):
+        if chk in range(0, rng):
             return True
+        else:
+            return False
+
+    print('-' * 20)  # spacer
 
     inpu = input('choose option: ')
 
-    while check(inpu, menu_range) == False:
+    while not check(inpu, menu_range):
         inpu = input('try again: ')
 
     return int(inpu)
+
+
+def add_to_collected(wtype, option):
+    """add collected options to list"""
+    collected.append('{} = {}'.format(wtype, option, ))
+    return
+
+
+def display_collected():
+    """Display selected options list"""
+    os.system('clear')  # clearscreen
+    print('BS4 widget generator')
+    print('-' * 20)
+    print('options selected:')
+    for col in collected:
+        print(col)
+
+    print('-' * 20)
+
+    return
+
 
 if __name__ == "__main__":
     main()
